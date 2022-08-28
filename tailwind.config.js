1de8ca2bec54b27a78a42cb8640c7470f7448536
JamalAlsofy <107511283+JamalAlsofy@users.noreply.github.com>
2022-08-29 02:11:53 +0300

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,24 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: [
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.neutral,
-      green: colors.green,
-      blue: colors.blue,
-      indigo: colors.indigo,
+    content: [
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.neutral,
+            green: colors.green,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            pink: colors.pink,
+            purple: colors.purple,
+        },
+        extend: {},
     },
-    extend: {
-    },
-  },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-  enabled: process.env.NODE_ENV === 'production',
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    enabled: process.env.NODE_ENV === 'production',
 }
